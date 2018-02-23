369a429b0b7552890741c1173d7feca6e23c342e
TMK1111 <31999134+TMK1111@users.noreply.github.com>
2018-02-23 20:22:09 +0200

    Update miosLoad.sh

--- bundles/binding/org.openhab.binding.mios/examples/scripts/miosLoad.sh
+++ bundles/binding/org.openhab.binding.mios/examples/scripts/miosLoad.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" != "1" ]
 then
-    echo "Usage: $0 <VeraIpAddress>"
+    echo "Usage: $0 <192.168.1.22>"
     exit 1
 else
     MIOS_IP=$1; export MIOS_IP
@@ -12,7 +12,7 @@ fi
 echo "Loading MiOS Unit Metadata from ${MIOS_IP}..."
 curl --max-time 15 \
      --output ${MIOS_OUT} \
-     --silent "http://${MIOS_IP}:49451/data_request?id=user_data&output_format=xml"
+     --silent "http://${192.168.1.22}:49451/data_request?id=user_data&output_format=xml"
 
 if [ "$?" == "0" ]
 then
